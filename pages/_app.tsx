926cde8ff75ec79674acbd904ba7b94bab052c36
Ralph Largo <devlargs@gmail.com>
2020-12-29 11:52:38 +0800

    feat: added firebase auth with google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,8 @@
 import "../styles/globals.css";
-import { signIn, signOut, useSession, signout } from "next-auth/client";
-import { sign } from "crypto";
-import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [session, loading] = useSession();
-
   return (
     <>
-      {!session && (
-        <>
-          Not signed in <br />
-          <button
-            onClick={async () => {
-              const a = await signIn();
-              console.log(a);
-            }}
-          >
-            Sign in
-          </button>
-        </>
-      )}
-      {session && (
-        <>
-          <>
-            {loading ? (
-              <>Loading</>
-            ) : (
-              <>
-                Signed in as {session.user.email} <br />
-              </>
-            )}
-          </>
-          <button
-            onClick={async () => {
-              signOut();
-            }}
-          >
-            Sign out
-          </button>
-        </>
-      )}
       <Component {...pageProps} />
     </>
   );
