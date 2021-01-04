2e6dd85e6e6217156e497ce59a914493052b9b56
Ralph Largo <devlargs@gmail.com>
2021-01-04 20:09:04 +0800

    feat: manual deploy (added condition console)

--- utils/isLocalhost.ts
+++ utils/isLocalhost.ts
@@ -3,6 +3,7 @@ export default () => () => {
     console.log(process.env.NODE_ENV, "node - env");
     return process.env.NODE_ENV === "development";
   } else {
+    console.log("fall here");
     return (
       location.hostname === "localhost" || location.hostname === "127.0.0.1"
     );
