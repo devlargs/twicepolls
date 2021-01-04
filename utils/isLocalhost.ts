66628e59598135bc3b1d5537a8b3edcfd86c59d4
Ralph Largo <devlargs@gmail.com>
2021-01-04 22:53:55 +0800

    chore: refactor isLocalhost function

--- utils/isLocalhost.ts
+++ utils/isLocalhost.ts
@@ -1,11 +1,6 @@
-export default () => {
-  if (typeof window !== "undefined") {
-    console.log(process.env.NODE_ENV, "node - env");
-    return process.env.NODE_ENV === "development";
-  } else {
-    console.log("fall here");
-    return (
-      location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    );
-  }
-};
+const isLocalhost = () =>
+  process.browser
+    ? location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    : process.env.NODE_ENV === "development";
+
+export default isLocalhost;
