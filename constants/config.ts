3a3ac8ce8130a01c1343a28f1022736352b71478
Ralph Largo <devlargs@gmail.com>
2021-01-05 17:45:13 +0800

    feat: added additional background images

--- constants/config.ts
+++ constants/config.ts
@@ -6,6 +6,10 @@ const local = isLocalhost();
 const bg = () => {
   const items = [
     "https://image.kpopmap.com/2019/04/ESTEE-LAUDER-TWICE-QUIZ-COVER.jpg",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hbz-twice-1-1579279628.jpg?crop=1.00xw:0.703xh;0,0.204xh&resize=1200:*",
+    "https://www.sbs.com.au/popasia/sites/sbs.com.au.popasia/files/styles/full/public/twice-5_0.jpg?itok=AXQ23M9z&mtime=1506487589",
+    "https://www.allkpop.com/upload/2020/06/content/052155/1591408535-20200605-twice.jpg",
+    "https://lh3.googleusercontent.com/GcKoh37h0qpSJM2IIMXLfNwUIjP5fMdG8R94VVmsyNLKj16JCh2f9hQ4wqr6RhXUOqsTcPSzYoA-XjFPoVO1jJwnk9O7570e2g=w1200-h630-rj-pp-e365",
   ];
   return items[Math.floor(Math.random() * items.length)];
 };
