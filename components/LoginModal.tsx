2980c6ddad4af444639974083c2ffdc67e31c88e
Ralph Largo <devlargs@gmail.com>
2021-01-22 00:10:29 +0800

    feat: static values in list of polls

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -52,12 +52,6 @@ const LoginModal = ({
               >
                 Please login to continue
               </h3>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Consequatur amet labore.
-                </p>
-              </div>
             </div>
           </div>
           <FirebaseAuth />
@@ -67,7 +61,7 @@ const LoginModal = ({
               type="button"
               className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
             >
-              Go back to dashboard
+              Cancel
             </button>
           </div>
         </div>
