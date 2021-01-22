0657b8751ad725dd552d95613ffb4322140e0a46
Ralph Largo <devlargs@gmail.com>
2021-01-22 11:32:22 +0800

    feat: added static chart

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -17,10 +17,14 @@ const Header = () => {
     setMenuVisibility(hovered);
   }, [hovered, setMenuVisibility]);
 
-  const getHeaderClass = (path: string) =>
-    pathname === path
-      ? "border-b-2 border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium"
-      : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium";
+  const getHeaderClass = (path: string) => {
+    if (path !== "/") {
+      return pathname === path || pathname.includes(path)
+        ? "active-tab"
+        : "inactive-tab";
+    }
+    return pathname === path ? "active-tab" : "inactive-tab";
+  };
 
   return (
     <>
