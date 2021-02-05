450bf253bb1fa3b612cbecd0fed4459de8ef0295
Ralph Largo <devlargs@gmail.com>
2021-02-05 21:42:26 +0800

    feat: added login to comment

--- components/PollComments.tsx
+++ components/PollComments.tsx
@@ -50,44 +50,63 @@ const PollComments = ({ pollId }: { pollId: string }) => {
           </ul>
         </div>
       </div>
-      <div className="bg-gray-50 px-4 py-6 sm:px-6">
-        <div className="flex space-x-3">
-          <div className="flex-shrink-0">
-            <img className="h-10 w-10 rounded-full" src={user?.avatar} alt="" />
-          </div>
-          <div className="min-w-0 flex-1">
-            <form>
-              <div>
-                <label htmlFor="comment" className="sr-only">
-                  About
-                </label>
-                <textarea
-                  id="comment"
-                  name="comment"
-                  rows={3}
-                  className="shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border-gray-300 rounded-md"
-                  placeholder="Add a comment"
-                ></textarea>
-              </div>
-              <div className="mt-3 flex items-center justify-between">
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    Swal.fire(
-                      "Beta Version",
-                      "Comment section is currently in progress",
-                      "info"
-                    );
-                  }}
-                  className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-700 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                >
-                  Comment
-                </button>
-              </div>
-            </form>
+
+      {user && user?.id ? (
+        <div className="bg-gray-50 px-4 py-6 sm:px-6">
+          <div className="flex space-x-3">
+            <div className="flex-shrink-0">
+              <img
+                className="h-10 w-10 rounded-full"
+                src={user?.avatar}
+                alt=""
+              />
+            </div>
+            <div className="min-w-0 flex-1">
+              <form>
+                <div>
+                  <label htmlFor="comment" className="sr-only">
+                    About
+                  </label>
+                  <textarea
+                    id="comment"
+                    name="comment"
+                    rows={3}
+                    className="shadow-sm p-4 block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border-gray-300 rounded-md"
+                    placeholder="Add a comment"
+                  ></textarea>
+                </div>
+                <div className="mt-3 flex items-center justify-between">
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      Swal.fire(
+                        "Beta Version",
+                        "Comment section is currently in progress",
+                        "info"
+                      );
+                    }}
+                    className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-700 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  >
+                    Comment
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="mt-3 ml-14 flex items-center justify-between">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              Swal.fire("Beta Version", "Functionality in progress", "info");
+            }}
+            className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-700 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mb-5"
+          >
+            Login to Comment
+          </button>
+        </div>
+      )}
     </div>
   );
 };
